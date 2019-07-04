69a588122c21327443e2a39050cebdc71b3ea49e
gmdmgithub <gmika@interia.pl>
2019-07-04 23:35:54 +0200

    pandas introduced to read and manipulate csv file

--- vertical_bar.py
+++ vertical_bar.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import csv
 from collections import Counter
+import pandas as pd
 
 def standadReader():
     with open('data2.csv') as csv_file:
@@ -13,24 +14,55 @@ def standadReader():
         for row in csv_reader:
             language_counter.update(row['LanguagesWorkedWith'].split(";"))
     
-    print(language_counter)
+    # print(language_counter)
+    # print(language_counter.most_common(10))
+
+    languages = []
+    popularity = []
+    for item in language_counter.most_common(12):
+        languages.append(item[0])
+        popularity.append(item[1])
+
+    return languages, popularity
+
+def pandasWay():
+    data = pd.read_csv('data2.csv')
+
+    ids = data['Responder_id']
+    lang_answers = data['LanguagesWorkedWith']
+    
+    language_counter = Counter()
+    for answer in lang_answers:
+        language_counter.update(answer.split(";"))
+
+    languages = []
+    popularity = []
+    for item in language_counter.most_common(15):
+        languages.append(item[0])
+        popularity.append(item[1])
+
+    return languages, popularity
 
 def main():
 
     plt.style.use('fivethirtyeight')
     
-    standadReader()
+    languages, popularity = pandasWay() #standadReader()
+
+    print(languages.reverse(), popularity.reverse())
     
+    plt.barh(languages, popularity)
 
-    # plt.xlabel('Ages')
-    # plt.ylabel('Median Salary (USD)')
-    # plt.title('Median Salary (USD) by Age')
 
-    # plt.legend()
+    # plt.ylabel('Languages')
+    plt.xlabel('Popularity')
+    plt.title('Popularity of programming')
 
-    # plt.tight_layout()
-    # plt.savefig('plot.png')
-    # plt.show()
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('plot-lang-pop.png')
+    plt.show()
 
 if __name__ == "__main__":
     main()
