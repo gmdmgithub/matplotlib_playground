61ae6c9aca98ea9c2c82c2e7e1886741110c22de
gmdmgithub <gmika@interia.pl>
2019-07-05 09:56:12 +0200

    pie chart with explode, and percentage

--- piechart.py
+++ piechart.py
@@ -1,20 +1,50 @@
 from matplotlib import pyplot as plt
 
-def main():
 
+def simplePie():
     plt.style.use('fivethirtyeight')
-    
-    slices = [20,30,10,40]
 
-    plt.pie(slices)
+    slices = [20, 30, 10, 30]
+    labels = ['good', 'enough', 'better', 'very good']
+    colors = ['blue', 'green', 'red', '#e5ae37']
+
+    plt.pie(slices, labels=labels, colors=colors,
+            wedgeprops={'edgecolor': 'black'})
 
     plt.title('Some data')
 
-    plt.legend()
+    # plt.legend()
 
     plt.tight_layout()
     plt.savefig('plot-piechart.png')
     plt.show()
+
+
+def programLangsPie():
+    plt.style.use('fivethirtyeight')
+
+    slices = [59219, 55466, 47544, 36443, 35917, 31991, 27097]
+    labels = ['JavaScript', 'HTML/CSS', 'SQL',
+              'Python', 'Java', 'Bash/Shell/PowerShell', 'C#']
+    explode = [0, 0, 0, 0.05, 0, 0, 0]
+
+    plt.pie(slices, labels=labels, explode=explode, shadow=True,
+            startangle=45, autopct='%1.1f%%', # set percentage on the piechart
+            wedgeprops=dict(edgecolor='black'))#made dict from values - nice
+
+    plt.title('Language popularity')
+
+    # plt.legend()
+
+    plt.tight_layout()
+    plt.savefig('plot-piechart-lp.png')
+    plt.show()
+
+
+def main():
+    # simplePie()
+    programLangsPie()
+
 
 if __name__ == "__main__":
     main()
